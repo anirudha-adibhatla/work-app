a434ce83ecc2d8b79ce7601b38e7d97b37dd8e6e
anirudha-adibhatla <anirudha.adibhatla@gmail.com>
2017-09-30 19:16:34 -0700

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSidenavModule} from '@angular/material';
+import {MatExpansionModule} from '@angular/material';
+import {MatListModule} from '@angular/material';
+import {MatCardModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { MainSideBarComponent } from './main-side-bar/main-side-bar.component';
+import { MainNavBarComponent } from './main-nav-bar/main-nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainSideBarComponent,
+    MainNavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatExpansionModule,
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
